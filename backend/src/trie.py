bed2092078f9c65a5e2f719a13081b2b18a3aeed
Agustinus Jason Tan Wijaya <wijayatanjason0@gmail.com>
2026-07-04 13:48:07 +0700

    FIX: changed incorrect func_dict hashing

--- backend/src/trie.py
+++ backend/src/trie.py
@@ -3,7 +3,7 @@ from .init import *
 root, func_dict = init_trie()
 
 def insert_word(word: str):
-    func_dict["insertWord"](root, word.encode("utf-8"))
+    func_dict["insertTrieNode"](root, word.encode("utf-8"))
 
 def search_word(word: str):
     pass
